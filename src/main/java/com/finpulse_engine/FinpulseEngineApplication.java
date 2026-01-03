d79912534054445fa649416cfaddea51843ad451
abhinavdubey8989 <abhinavdubey8989@gmail.com>
2026-01-03 12:27:27 +0530

    1st commit

---
+++ src/main/java/com/finpulse_engine/FinpulseEngineApplication.java
@@ -0,0 +1,13 @@
+package com.finpulse_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinpulseEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinpulseEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finpulse_engine/FinpulseEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finpulse_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinpulseEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
